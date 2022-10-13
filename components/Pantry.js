a8332846d85a007dbe5297e1a52f6cda584e202d
LucaSalmi <luca.salmi90@gmail.com>
2022-10-13 17:08:29 +0200

    started work on pantry

--- components/Pantry.js
+++ components/Pantry.js
@@ -1,22 +1,37 @@
 import { StyleSheet, Text, View, TextInput, Button, ScrollView } from 'react-native';
 import { useState } from 'react';
+import { Card } from './Card';
+import { smallCardStyles } from '../styles/styles';
 
-const Pantry = (props) => {
+pantryItems = [0,1,2,3,4]
 
-    
+const Pantry = (props) => {  
 
-    const showPerson = () => {
-        console.log(props.props1.styles.text);
-    }   
-
-	return (
-        <View>
-            <Text>Hello {props.props1.name}!</Text>
-            <Text style={props.props1.styles.text}>Click Me</Text>
-            <Button title='Log something' onPress={showPerson}></Button>
-        </View>
-        
-	);
+    return(
+        <FullPantry></FullPantry>
+    )
 }
+
+const EmptyPantry = () =>{
+    return(
+        <View>
+        <Text>It looks empty in here! Start adding items to your pantry!</Text>
+        <Button title='add' onPress={()=>{
+            let i = 'apple';
+            setPantryItems([0,1,2,3]);
+        }}></Button>
+        </View>
+    )
+}
+
+const FullPantry = () =>{
+    return(
+
+        <ScrollView>
+                {pantryItems.map((item, i)=><Card title={item} style={smallCardStyles.container} isSmallCard={true} />)}
+        </ScrollView>
+    )
+}
+
 
 export default Pantry;
