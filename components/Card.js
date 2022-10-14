9069afcc56f4cb94af3be6cd5ddc249f47f1a3dc
officialskum <95219641+officialskum@users.noreply.github.com>
2022-10-14 15:26:21 +0200

    detailsViewGradientAndMore

--- components/Card.js
+++ components/Card.js
@@ -1,28 +1,36 @@
-import {StyleSheet, Text, View, TouchableOpacity } from 'react-native';
-import { bigCardStyles, smallCardStyles } from '../styles/styles';
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import { bigCardStyles, smallCardStyles } from "../styles/styles";
 import Icon from "react-native-ico-material-design";
 
 export function BigCard(myProps) {
-
-    return (
-        <View style={myProps.isSmallCard ? [smallCardStyles.superView] : [bigCardStyles.superView]}>
-          <View style={[myProps.style, bigCardStyles.elevation]}>
-          <TouchableOpacity style={myProps.topCard} >
-                <Icon name="favorite-heart-outline-button" group="material-design" height="38" width="38"/>
-            </TouchableOpacity>
+  return (
+    <View
+      style={
+        myProps.isSmallCard
+          ? [smallCardStyles.superView]
+          : [bigCardStyles.superView]
+      }
+    >
+      <View style={[myProps.style, bigCardStyles.elevation]}>
+        <TouchableOpacity style={myProps.topCard}>
+          <Icon
+            name="favorite-heart-outline-button"
+            group="material-design"
+            height="38"
+            width="38"
+          />
+        </TouchableOpacity>
 
         <View style={bigCardStyles.bottomCard}>
-            <View style={bigCardStyles.kokTid}>
+          <View style={bigCardStyles.kokTid}>
             <Icon name="list-button-with-3-elements" group="material-design" />
-            <Text style={{marginLeft: 7}}>{myProps.cookingTime}</Text>
-            </View>
-            <Text style={bigCardStyles.dishName}>{myProps.title}</Text>
+            <Text style={{ marginLeft: 7 }}>{myProps.cookingTime}</Text>
+          </View>
+          <Text style={bigCardStyles.dishName}>{myProps.title}</Text>
         </View>
-
-        </View>  
-        </View>
-        
-    );
+      </View>
+    </View>
+  );
 }
 
 /*
@@ -40,14 +48,11 @@ export function BigCard(myProps) {
 */
 
 export function SmallCard(myProps) {
-
-    return (
-        <View style={smallCardStyles.superView}>
-          <View style={[smallCardStyles.container, bigCardStyles.elevation]}>
-            <Text>{myProps.title}</Text>
-        </View>  
-        </View>
-        
-    );
+  return (
+    <View style={smallCardStyles.superView}>
+      <View style={[smallCardStyles.container, bigCardStyles.elevation]}>
+        <Text>{myProps.title}</Text>
+      </View>
+    </View>
+  );
 }
-
