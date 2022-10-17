b6de55a8a438e2ef955e70ed03b58fc21c849f02
officialskum <95219641+officialskum@users.noreply.github.com>
2022-10-17 10:17:37 +0200

    clickableHeartsandTimer

--- App.js
+++ App.js
@@ -1,51 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { Card } from './components/Card';
-import { StyleSheet, Text, View, Button, SafeAreaView, Platform, TouchableOpacity, SectionList } from 'react-native';
-import Recipes from './components/Recipes.js';
-import Favorite from './components/Favorite.js';
-import Shoplist from './components/Shoplist.js';
-import Pantry from './components/Pantry.js';
-import Profile from './components/Profile.js';
-import { pageStyles, bigCardStyles } from './styles/styles.js';
-import { useState, useEffect } from 'react';
+import { StatusBar } from "expo-status-bar";
+import { Card } from "./components/Card";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  SafeAreaView,
+  Platform,
+  TouchableOpacity,
+  SectionList,
+} from "react-native";
+import Recipes from "./components/Recipes.js";
+import Favorite from "./components/Favorite.js";
+import Shoplist from "./components/Shoplist.js";
+import Pantry from "./components/Pantry.js";
+import Profile from "./components/Profile.js";
+import { pageStyles, bigCardStyles } from "./styles/styles.js";
+import { useState, useEffect } from "react";
 import Icon from "react-native-ico-material-design";
-import RecipeDetails from './components/RecipeDetails';
+import RecipeDetails from "./components/RecipeDetails";
 
 //testing more hello
 //a comment from ankan, hello guys
 //dev WHAT
 
-
 export default function App() {
-
   if (Platform.OS == "android") {
-    return (
-
-      <MainContent />
-
-    );
-  }
-  else {
-
+    return <MainContent />;
+  } else {
     return (
       <SafeAreaView style={{ flex: 1 }}>
         <MainContent />
       </SafeAreaView>
     );
   }
-
 }
 
 const MainContent = () => {
-
   const RECIPES = 0;
   const FAVORITE = 1;
   const SHOPLIST = 2;
   const PANTRY = 3;
   const PROFILE = 4;
-  const RECIPEDETAILS = 5; 
+  const RECIPEDETAILS = 5;
 
-  const previousScreen = RECIPES; 
+  const previousScreen = RECIPES;
 
   const [screen, setScreen] = useState(RECIPES);
 
@@ -54,105 +53,118 @@ const MainContent = () => {
   let view;
 
   switch (screen) {
-
     case RECIPES:
-      view = <Recipes setScreen={setScreen} detailId={RECIPEDETAILS}/>
+      view = <Recipes setScreen={setScreen} detailId={RECIPEDETAILS} />;
       break;
 
     case FAVORITE:
-      view = <Favorite props1={{ name: "test", styles: pageStyles }} />
+      view = <Favorite props1={{ name: "test", styles: pageStyles }} />;
       break;
 
     case SHOPLIST:
-      view = <Shoplist navBarChanger={setHideNav} />
+      view = <Shoplist navBarChanger={setHideNav} />;
       break;
 
     case PANTRY:
-      view = <Pantry navBarChanger={setHideNav} />
+      view = <Pantry navBarChanger={setHideNav} />;
       break;
 
     case PROFILE:
-      view = <Profile props1={{ name: "test", styles: pageStyles }} />
+      view = <Profile props1={{ name: "test", styles: pageStyles }} />;
       break;
 
-    case RECIPEDETAILS: 
-      view = <RecipeDetails setScreen={setScreen} previousPage={previousScreen}/>
-      break; 
-
+    case RECIPEDETAILS:
+      view = (
+        <RecipeDetails setScreen={setScreen} previousPage={previousScreen} />
+      );
+      break;
   }
 
   const changePage = (pageName) => {
     setScreen(pageName);
-  }
+  };
 
   return (
     <View style={styles.container}>
-
-      <View style={styles.currentPage}>
-        {view}
-      </View>
+      <View style={styles.currentPage}>{view}</View>
       <View style={hideNav ? styles.navBarHidden : styles.navBar}>
-        <TouchableOpacity style={styles.navButton} onPress={() => { changePage(RECIPES) }}>
-          <Icon name="list-button-with-3-elements" group="material-design"/>
+        <TouchableOpacity
+          style={styles.navButton}
+          onPress={() => {
+            changePage(RECIPES);
+          }}
+        >
+          <Icon name="list-button-with-3-elements" group="material-design" />
           <Text>Recipes</Text>
         </TouchableOpacity>
 
-        <TouchableOpacity style={styles.navButton} onPress={() => { changePage(FAVORITE) }}>
+        <TouchableOpacity
+          style={styles.navButton}
+          onPress={() => {
+            changePage(FAVORITE);
+          }}
+        >
           <Icon name="favorite-heart-outline-button" group="material-design" />
           <Text>Favorites</Text>
         </TouchableOpacity>
 
-        <TouchableOpacity style={styles.navButton} onPress={() => { changePage(SHOPLIST) }}>
+        <TouchableOpacity
+          style={styles.navButton}
+          onPress={() => {
+            changePage(SHOPLIST);
+          }}
+        >
           <Icon name="shopping-cart" group="basic" />
           <Text>Shoplist</Text>
         </TouchableOpacity>
 
-        <TouchableOpacity style={styles.navButton} onPress={() => { changePage(PANTRY) }}>
-          <Icon name="rounded-add-button" group="material-design"/>
+        <TouchableOpacity
+          style={styles.navButton}
+          onPress={() => {
+            changePage(PANTRY);
+          }}
+        >
+          <Icon name="rounded-add-button" group="material-design" />
           <Text>Pantry</Text>
         </TouchableOpacity>
 
-        <TouchableOpacity style={styles.navButton} onPress={() => { changePage(PROFILE) }}>
-          <Icon name="user-outline" group="material-design"/>
+        <TouchableOpacity
+          style={styles.navButton}
+          onPress={() => {
+            changePage(PROFILE);
+          }}
+        >
+          <Icon name="user-outline" group="material-design" />
           <Text>Profile</Text>
         </TouchableOpacity>
-
-
       </View>
 
       <StatusBar hidden={false} backgroundColor="white" translucent={false} />
     </View>
   );
-}
-
-
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    width: '100%',
-    backgroundColor: '#fff',
-
+    width: "100%",
+    backgroundColor: "#fff",
   },
   currentPage: {
     flex: 1,
   },
   navBar: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    backgroundColor: '#F3F3F3',
+    flexDirection: "row",
+    justifyContent: "space-around",
+    backgroundColor: "#F3F3F3",
     paddingBottom: 18,
     paddingTop: 18,
-
-
   },
   navBarHidden: {
-    display: 'none',
+    display: "none",
   },
   navButton: {
-    flexDirection: 'column',
-    alignItems: 'center'
-
-
+    flexDirection: "column",
+    alignItems: "center",
   },
 });
--- components/Card.js
+++ components/Card.js
@@ -1,9 +1,20 @@
-
-import { bigCardStyles, smallCardStyles, pantryCardStyles } from '../styles/styles';
-import {StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import {
+  bigCardStyles,
+  smallCardStyles,
+  pantryCardStyles,
+} from "../styles/styles";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import Icon from "react-native-ico-material-design";
+import { useState, useEffect } from "react";
 
 export function BigCard(myProps) {
+  const [heartEmpty, setFillHeart] = useState(true);
+
+  const toggleHeart = () => {
+    //let toggle = !heartEmpty;
+    setFillHeart((current) => !current);
+  };
+
   return (
     <View
       style={
@@ -13,9 +24,18 @@ export function BigCard(myProps) {
       }
     >
       <View style={[myProps.style, bigCardStyles.elevation]}>
-        <TouchableOpacity style={myProps.topCard}>
+        <TouchableOpacity
+          style={myProps.topCard}
+          onPress={() => {
+            toggleHeart();
+          }}
+        >
           <Icon
-            name="favorite-heart-outline-button"
+            name={
+              heartEmpty
+                ? "favorite-heart-outline-button"
+                : "favorite-heart-button"
+            }
             group="material-design"
             height="38"
             width="38"
@@ -24,7 +44,7 @@ export function BigCard(myProps) {
 
         <View style={bigCardStyles.bottomCard}>
           <View style={bigCardStyles.kokTid}>
-            <Icon name="list-button-with-3-elements" group="material-design" />
+            <Icon name="set-timer-button" group="material-design" />
             <Text style={{ marginLeft: 7 }}>{myProps.cookingTime}</Text>
           </View>
           <Text style={bigCardStyles.dishName}>{myProps.title}</Text>
@@ -49,26 +69,24 @@ export function BigCard(myProps) {
 */
 
 export function SmallCard(myProps) {
-    return (
-        <View style={smallCardStyles.superView}>
-            <View style={[smallCardStyles.container, bigCardStyles.elevation]}>
-                <Text>{myProps.title}</Text>
-            </View>
-        </View>
-
-    );
+  return (
+    <View style={smallCardStyles.superView}>
+      <View style={[smallCardStyles.container, bigCardStyles.elevation]}>
+        <Text>{myProps.title}</Text>
+      </View>
+    </View>
+  );
 }
 
 export function PantryCard(myProps) {
-    return (
-        <View style={pantryCardStyles.superView}>
-            <View style={[pantryCardStyles.container, bigCardStyles.elevation]}>
-                <Text>{myProps.item.quantity}</Text>
-                <Text>{myProps.item.measure}</Text>
-                <Text> </Text>
-                <Text>{myProps.item.title}</Text>
-            </View>
-        </View>
-
-    );
+  return (
+    <View style={pantryCardStyles.superView}>
+      <View style={[pantryCardStyles.container, bigCardStyles.elevation]}>
+        <Text>{myProps.item.quantity}</Text>
+        <Text>{myProps.item.measure}</Text>
+        <Text> </Text>
+        <Text>{myProps.item.title}</Text>
+      </View>
+    </View>
+  );
 }
