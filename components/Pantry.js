1388649be2377e72810b0d6b2f9f4c5342d4cc61
LucaSalmi <luca.salmi90@gmail.com>
2022-10-18 15:21:55 +0200

    added tutrorial message when no other items are present

--- components/Pantry.js
+++ components/Pantry.js
@@ -14,6 +14,10 @@ const Pantry = (props) => {
     const [isVisible, setisVisible] = useState(false);
     const [searchText, setSearchText] = useState();
     const [itemToAdd, setItemToAdd] = useState();
+    const [foundItem, setFoundItem] = useState([]);
+    const [pantryItems, setPantryItems] = useState([
+        new PantryItem(-1, " by clicking the button below", "adding", "Start ")
+    ]);
 
     const radioData = [
         {
@@ -36,7 +40,6 @@ const Pantry = (props) => {
         },
     ];
 
-
     const toggleSheet = () => {
         let newBool = !showSheet;
         setShowSheet(newBool);
@@ -48,10 +51,6 @@ const Pantry = (props) => {
         let newBool = !isVisible;
         setisVisible(newBool);
     };
-
-    const [foundItem, setFoundItem] = useState([]);
-
-    const [pantryItems, setPantryItems] = useState([]);
 
     const pantryItemCard = ({ item }) => (
         <PantryCard item={item} />
@@ -79,18 +78,18 @@ const Pantry = (props) => {
     //Alert to confirm the elimination of an item in the pantry
     const deleteItemAlert = (props) => {
         Alert.alert(
-            "Deleting Item",
-            "Do you really want to remove " + props.item.title + " from your pantry?",
+            props.item.quantity + props.item.measure + props.item.title,
+            "What do You want to do?",
             [
                 {
-                    text: "No",
+                    text: "Return",
                     style: "cancel",
                     onPress: () => {
                         console.log(props.item)
                     }
                 },
                 {
-                    text: "Yes", onPress: () => {
+                    text: "Delete", onPress: () => {
                         let x = [];
                         for (const ingredient of pantryItems) {
                             if (ingredient.id != props.item.id) {
@@ -99,6 +98,11 @@ const Pantry = (props) => {
                             }
                         }
                         setPantryItems(x);
+                    }
+                },
+                {
+                    text: "Modify", onPress: () => {
+                        //toggleModal();
                     }
                 }
             ]
@@ -116,6 +120,16 @@ const Pantry = (props) => {
                 },
             ]
         );
+    }
+
+    function removeTutorial(){
+        let array = [];
+        for(item of pantryItems){
+            if(item.id != -1){
+                array.push(item)
+            }
+        }
+        return array;
     }
 
     //The modal that houses the quantity inputs for new item added to the pantry
@@ -163,7 +177,7 @@ const Pantry = (props) => {
                                 disabled={saveInactive}
                                 onPress={() => {
 
-                                    let i = pantryItems;
+                                    let i = removeTutorial();
                                     i.push(itemToAdd);
                                     setPantryItems(i);
                                     toggleReadyToSave();
@@ -175,7 +189,6 @@ const Pantry = (props) => {
                                 onPress={() => {
                                     toggleModal();
                                     toggleReadyToSave();
-
                                 }}
                             />
                         </View>
@@ -208,7 +221,7 @@ const Pantry = (props) => {
         return (
             <View style={pantryCardStyles.superView}>
                 <View onTouchStart={() => {
-                    //let i = pantryItems;
+
                     for (const item of pantryItems) {
 
                         if (item.title == myProps.title) {
@@ -260,7 +273,7 @@ const Pantry = (props) => {
 
             />
 
-            {/* View in the Sheet to search and ass new items to pantry */}
+            {/* View in the Sheet to search and add new items to pantry */}
             <KeyboardAvoidingView
                 style={showSheet ? shoplistPage.sheetContainer : { display: "none" }}
                 behavior={Platform.OS === "ios" ? "padding" : "height"}>
